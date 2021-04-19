5922b6c75958d98629d779e11874e58a8ce18010
funnzz <dumitru.aa@gmail.com>
2021-04-19 03:46:37 +0300

    initial commit

---
+++ src/main/java/my/homeprojects/springms/SpringmsApplication.java
@@ -0,0 +1,13 @@
+package my.homeprojects.springms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmsApplication.class, args);
+	}
+
+}
